c5039a3b763a363447ce76b6bb1075009e560fa6
Jimmy Ghettuba <ian.ghettuba@gmail.com>
2020-11-10 12:00:09 -0700

    Added WebApp Folders to project

---
+++ A03-Team-B-Project-ERace-WebApp/ERace-WebApp/SubSystems/Purchasing/Dummy3.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ERace_WebApp.SubSystems.Purchasing
+{
+    public class Dummy3
+    {
+    }
+}
