f06acc1845a4a515f929dfd2203a986470bddbff
Jimmy Ghettuba <ian.ghettuba@gmail.com>
2020-11-10 11:51:48 -0700

    Created ERace solution --Updated Nugget Packages --Built dependancies between library and Web App projects --Added bootstrap menu to site master

---
+++ A03-Team-B-Project-ERace-WebApp/ERace-WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ERace_WebApp.Global" Language="C#" %>
---
+++ A03-Team-B-Project-ERace-WebApp/ERace-WebApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ERace_WebApp.Startup))]
+namespace ERace_WebApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
