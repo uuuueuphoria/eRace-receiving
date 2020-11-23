092c574956dbdc9b4b8e9484b94fb09669429d29
Jimmy Ghettuba <ian.ghettuba@gmail.com>
2020-11-23 15:39:51 -0700

    ---Commented out sales pages security checks

--- A03-Team-B-Project-ERace-WebApp/ERace-WebApp/SubSystems/Sales/ReturnRefunds.aspx.cs
+++ A03-Team-B-Project-ERace-WebApp/ERace-WebApp/SubSystems/Sales/ReturnRefunds.aspx.cs
@@ -14,45 +14,45 @@ namespace ERace_WebApp.SubSystems.Sales
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //test our security
-            //are you logged in?
-            if (Request.IsAuthenticated)
-            {
-                if (User.IsInRole("Clerk"))
-                {
+            ////test our security
+            ////are you logged in?
+            //if (Request.IsAuthenticated)
+            //{
+            //    if (User.IsInRole("Clerk"))
+            //    {
 
-                    //obtain the CustomerId on the security User record
-                    SecurityController ssysmgr = new SecurityController();
-                    int? employeeid = ssysmgr.GetCurrentUserEmployeeId(User.Identity.Name);
+            //        //obtain the CustomerId on the security User record
+            //        SecurityController ssysmgr = new SecurityController();
+            //        int? employeeid = ssysmgr.GetCurrentUserEmployeeId(User.Identity.Name);
 
-                    //need to convert the int? to an int for the call to the CustomerController method
-                    //int custid = customerid == null ? default(int) : int.Parse(customerid.ToString());
-                    int emplid = employeeid ?? default(int);
+            //        //need to convert the int? to an int for the call to the CustomerController method
+            //        //int custid = customerid == null ? default(int) : int.Parse(customerid.ToString());
+            //        int emplid = employeeid ?? default(int);
 
-                    MessageUserControl.TryRun(() =>
-                    {
-                        EmployeeController csysmgr = new EmployeeController();
-                        EmployeeItem item = csysmgr.Employee_FindByID(emplid);
-                        if (item == null)
-                        {
-                            LoggedUser.Text = "Unknown";
-                            throw new Exception("Logged employee cannot be found on file ");
-                        }
-                        else
-                        {
-                            LoggedUser.Text = item.LastName + ", " + item.FirstName;
-                        }
-                    });
-                }
-                else
-                {
-                    Response.Redirect("~/SubSystems/Sales/AccessDenied.aspx");
-                }
-            }
-            else
-            {
-                Response.Redirect("~/Account/Login.aspx");
-            }
+            //        MessageUserControl.TryRun(() =>
+            //        {
+            //            EmployeeController csysmgr = new EmployeeController();
+            //            EmployeeItem item = csysmgr.Employee_FindByID(emplid);
+            //            if (item == null)
+            //            {
+            //                LoggedUser.Text = "Unknown";
+            //                throw new Exception("Logged employee cannot be found on file ");
+            //            }
+            //            else
+            //            {
+            //                LoggedUser.Text = item.LastName + ", " + item.FirstName;
+            //            }
+            //        });
+            //    }
+            //    else
+            //    {
+            //        Response.Redirect("~/SubSystems/Sales/AccessDenied.aspx");
+            //    }
+            //}
+            //else
+            //{
+            //    Response.Redirect("~/Account/Login.aspx");
+            //}
 
         }
     }
